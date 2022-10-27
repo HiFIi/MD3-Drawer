91fa63fe014d6d782465a3917f256a7912e77be4
Hifii <mbqsniper@hotmail.com>
2022-10-27 07:50:08 -0700

    fix the header text padding, make the app portrait mode only, format some code, add a to do, etc

--- app/src/main/java/io/hifii/drawertest/MainActivity.java
+++ app/src/main/java/io/hifii/drawertest/MainActivity.java
@@ -38,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        /**
+         * TO DO!!
+         *
+         * <p>I NEED TO OPEN THIS IN ANDROID STUDIO AND FIX UP SOME DEPRECATED STUFF!! *
+         */
         if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
         }
@@ -48,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
-        // make fully Android Transparent Status bar
+
+        // make the status bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
