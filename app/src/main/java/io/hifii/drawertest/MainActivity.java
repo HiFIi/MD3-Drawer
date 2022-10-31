4bd2e7caead0f2f2f20fac08ce7a1169c4abe91f
Hifii <mbqsniper@hotmail.com>
2022-10-31 16:47:52 -0700

    add an animated imageview and change up the home image height. just experimenting

--- app/src/main/java/io/hifii/drawertest/MainActivity.java
+++ app/src/main/java/io/hifii/drawertest/MainActivity.java
@@ -1,5 +1,7 @@
 package io.hifii.drawertest;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -9,7 +11,9 @@ import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -17,12 +21,10 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.color.DynamicColors;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.shape.MaterialShapeDrawable;
 import com.google.android.material.snackbar.Snackbar;
-
 import io.hifii.drawertest.MainActivity;
 import io.hifii.drawertest.databinding.ActivityMainBinding;
 
@@ -30,10 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
   private AppBarConfiguration mAppBarConfiguration;
   private ActivityMainBinding binding;
+  public ImageView iv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    // ImageView iv;
 
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
@@ -43,24 +47,11 @@ public class MainActivity extends AppCompatActivity {
      *
      * <p>I NEED TO OPEN THIS IN ANDROID STUDIO AND FIX UP SOME DEPRECATED STUFF!! *
      */
-    if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-      setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
-    }
-    if (Build.VERSION.SDK_INT >= 19) {
-      getWindow()
-          .getDecorView()
-          .setSystemUiVisibility(
-              View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    }
 
     // make the status bar transparent
-    if (Build.VERSION.SDK_INT >= 21) {
-      setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-      getWindow().setStatusBarColor(Color.TRANSPARENT);
-    }
 
-    //   getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
+  //   getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+     
     setSupportActionBar(binding.appBarMain.toolbar);
     binding.appBarMain.fab.setOnClickListener(
         new View.OnClickListener() {
@@ -74,6 +65,24 @@ public class MainActivity extends AppCompatActivity {
 
     float radius = getResources().getDimension(R.dimen.roundcorner);
     NavigationView navigationView2 = findViewById(R.id.nav_view);
+    // iv = (ImageView) findViewById(R.id.te);
+    final ImageView iv = (ImageView) findViewById(R.id.te);
+
+    ValueAnimator valueAnimator = ValueAnimator.ofFloat(1f, 1.65f);
+    valueAnimator.setDuration(5000);
+    valueAnimator.addUpdateListener(
+        new ValueAnimator.AnimatorUpdateListener() {
+          @Override
+          public void onAnimationUpdate(ValueAnimator animation) {
+            iv.setScaleX((Float) animation.getAnimatedValue());
+            iv.setScaleY((Float) animation.getAnimatedValue());
+          }
+        });
+    valueAnimator.setRepeatCount(Animation.INFINITE);
+    valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+    valueAnimator.start();
+    valueAnimator.start();
+
     MaterialShapeDrawable navViewBackground =
         (MaterialShapeDrawable) navigationView2.getBackground();
     navViewBackground.setShapeAppearanceModel(
