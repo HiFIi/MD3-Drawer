7c2b991082a9439cc8cf73df784b45023be6966f
Hifii <mbqsniper@hotmail.com>
2022-10-31 13:06:20 -0700

    organize most of the files, add some proguard/gradle.properties flags, etc

--- app/src/main/java/io/hifii/drawertest/MainActivity.java
+++ app/src/main/java/io/hifii/drawertest/MainActivity.java
@@ -28,103 +28,102 @@ import io.hifii.drawertest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+  private AppBarConfiguration mAppBarConfiguration;
+  private ActivityMainBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
 
-        /**
-         * TO DO!!
-         *
-         * <p>I NEED TO OPEN THIS IN ANDROID STUDIO AND FIX UP SOME DEPRECATED STUFF!! *
-         */
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            getWindow()
-                    .getDecorView()
-                    .setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
-        // make the status bar transparent
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-
-     //   getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                                .setAction("Action", null)
-                                .show();
-                    }
-                });
-
-        float radius = getResources().getDimension(R.dimen.roundcorner);
-        NavigationView navigationView2 = findViewById(R.id.nav_view);
-        MaterialShapeDrawable navViewBackground =
-                (MaterialShapeDrawable) navigationView2.getBackground();
-        navViewBackground.setShapeAppearanceModel(
-                navViewBackground.getShapeAppearanceModel().toBuilder()
-                        .setTopRightCorner(16, radius)
-                        .setBottomRightCorner(16, radius)
-                        .build());
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration =
-                new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                        .setOpenableLayout(drawer)
-                        .build();
-        NavController navController =
-                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+    /**
+     * TO DO!!
+     *
+     * <p>I NEED TO OPEN THIS IN ANDROID STUDIO AND FIX UP SOME DEPRECATED STUFF!! *
+     */
+    if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+      setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+    }
+    if (Build.VERSION.SDK_INT >= 19) {
+      getWindow()
+          .getDecorView()
+          .setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
-
-        Window win = activity.getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
-        win.setAttributes(winParams);
+    // make the status bar transparent
+    if (Build.VERSION.SDK_INT >= 21) {
+      setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+      getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    //   getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController =
-                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+    setSupportActionBar(binding.appBarMain.toolbar);
+    binding.appBarMain.fab.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show();
+          }
+        });
 
-    @Override
-    public String toString() {
-        return "MainActivity[mAppBarConfiguration=" + mAppBarConfiguration + "]";
+    float radius = getResources().getDimension(R.dimen.roundcorner);
+    NavigationView navigationView2 = findViewById(R.id.nav_view);
+    MaterialShapeDrawable navViewBackground =
+        (MaterialShapeDrawable) navigationView2.getBackground();
+    navViewBackground.setShapeAppearanceModel(
+        navViewBackground.getShapeAppearanceModel().toBuilder()
+            .setTopRightCorner(16, radius)
+            .setBottomRightCorner(16, radius)
+            .build());
+    DrawerLayout drawer = binding.drawerLayout;
+    NavigationView navigationView = binding.navView;
+    // Passing each menu ID as a set of Ids because each
+    // menu should be considered as top level destinations.
+    mAppBarConfiguration =
+        new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+            .setOpenableLayout(drawer)
+            .build();
+    NavController navController =
+        Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+    NavigationUI.setupWithNavController(navigationView, navController);
+  }
+
+  public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+
+    Window win = activity.getWindow();
+    WindowManager.LayoutParams winParams = win.getAttributes();
+    if (on) {
+      winParams.flags |= bits;
+    } else {
+      winParams.flags &= ~bits;
     }
+    win.setAttributes(winParams);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    NavController navController =
+        Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+        || super.onSupportNavigateUp();
+  }
+
+  @Override
+  public String toString() {
+    return "MainActivity[mAppBarConfiguration=" + mAppBarConfiguration + "]";
+  }
 }
