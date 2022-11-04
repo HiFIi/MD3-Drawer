34d579c8052bcf967748cef6af6a13c4680e07eb
Hifii <mbqsniper@hotmail.com>
2022-11-04 03:43:36 -0700

    set image to 16:9 and moved it into the homefragment, couple temp. theme changes, organize the code, etc -- WIP

--- app/src/main/java/io/hifii/drawertest/MainActivity.java
+++ app/src/main/java/io/hifii/drawertest/MainActivity.java
@@ -1,149 +1,170 @@
 package io.hifii.drawertest;
 
-import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import androidx.appcompat.app.*;
-import androidx.activity.*;
 import android.app.Activity;
-import android.content.Context;
+import android.app.Application;
 import android.graphics.Color;
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
+import android.transition.Fade;
+import android.transition.Transition;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowInsets;
 import android.view.WindowManager;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.transition.Fade;
-import androidx.transition.Transition;
 import com.google.android.material.color.DynamicColors;
+import com.google.android.material.color.DynamicColorsOptions;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.shape.MaterialShapeDrawable;
 import com.google.android.material.snackbar.Snackbar;
-import io.hifii.drawertest.MainActivity;
 import io.hifii.drawertest.databinding.ActivityMainBinding;
+import io.hifii.drawertest.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  private AppBarConfiguration mAppBarConfiguration;
-  private ActivityMainBinding binding;
-  public ImageView iv;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+    public ImageView iv;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // ImageView iv;
+    public static final int ll = 500;
 
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
-    setContentView(binding.getRoot());
+    public NavigationView nv;
 
-    /**
-     * TO DO!!
-     *
-     * <p>I NEED TO OPEN THIS IN ANDROID STUDIO AND FIX UP SOME DEPRECATED STUFF!! *
-     */
+    public DrawerLayout drawer;
 
-    // make the status bar transparent
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    //   getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    setSupportActionBar(binding.appBarMain.toolbar);
-    binding.appBarMain.fab.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .show();
-          }
-        });
+        /**
+         * TO DO!!
+         *
+         * <p>I NEED TO OPEN THIS IN ANDROID STUDIO AND FIX UP SOME DEPRECATED STUFF!! *
+         */
 
-    float radius = getResources().getDimension(R.dimen.roundcorner);
-    NavigationView navigationView2 = findViewById(R.id.nav_view);
-    // iv = (ImageView) findViewById(R.id.te);
-    final ImageView iv = (ImageView) findViewById(R.id.te);
+        // make the status bar transparent
 
-    ValueAnimator valueAnimator = ValueAnimator.ofFloat(1.1f, 1.65f);
-    valueAnimator.setDuration(5000);
-    valueAnimator.addUpdateListener(
-        new ValueAnimator.AnimatorUpdateListener() {
-          @Override
-          public void onAnimationUpdate(ValueAnimator animation) {
-            iv.setScaleX((Float) animation.getAnimatedValue());
-            iv.setScaleY((Float) animation.getAnimatedValue());
-          }
-        });
-    valueAnimator.setRepeatCount(Animation.INFINITE);
-    valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
-    valueAnimator.start();
-    valueAnimator.start();
+        //   getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
-    Transition fade = new Fade();
-    fade.excludeTarget(android.R.id.statusBarBackground, true);
-    fade.excludeTarget(android.R.id.navigationBarBackground, true);
-    getWindow().setExitTransition(null);
-    getWindow().setEnterTransition(null);
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null)
+                                .show();
+                    }
+                });
 
-    // DynamicColors.applyToActivitiesIfAvailable(this);
-    MaterialShapeDrawable navViewBackground =
-        (MaterialShapeDrawable) navigationView2.getBackground();
-    navViewBackground.setShapeAppearanceModel(
-        navViewBackground.getShapeAppearanceModel().toBuilder()
-            .setTopRightCorner(16, radius)
-            .setBottomRightCorner(16, radius)
-            .build());
-    DrawerLayout drawer = binding.drawerLayout;
-    NavigationView navigationView = binding.navView;
-    // Passing each menu ID as a set of Ids because each
-    // menu should be considered as top level destinations.
-    mAppBarConfiguration =
-        new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-            .setOpenableLayout(drawer)
-            .build();
-    NavController navController =
-        Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-    NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-    NavigationUI.setupWithNavController(navigationView, navController);
-  }
+        float radius = getResources().getDimension(R.dimen.roundcorner);
+        NavigationView navigationView2 = findViewById(R.id.nav_view);
 
-  public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        MaterialShapeDrawable navViewBackground =
+                (MaterialShapeDrawable) navigationView2.getBackground();
+        navViewBackground.setShapeAppearanceModel(
+                navViewBackground.getShapeAppearanceModel().toBuilder()
+                        .setTopRightCorner(16, radius)
+                        .setBottomRightCorner(16, radius)
+                        .build());
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        navigationView.setDrawBottomInsetForeground(true);
 
-    Window win = activity.getWindow();
-    WindowManager.LayoutParams winParams = win.getAttributes();
-    if (on) {
-      winParams.flags |= bits;
-    } else {
-      winParams.flags &= ~bits;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                        .setOpenableLayout(drawer)
+                        .build();
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
-    win.setAttributes(winParams);
-  }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-  }
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
 
-  @Override
-  public boolean onSupportNavigateUp() {
-    NavController navController =
-        Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-    return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-        || super.onSupportNavigateUp();
-  }
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
 
-  @Override
-  public String toString() {
-    return "MainActivity[mAppBarConfiguration=" + mAppBarConfiguration + "]";
-  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        ScrimInsetsFrameLayout mainContent = findViewById(R.id.main_content);
+        //noinspection StatementWithEmptyBody
+        if (mainContent != null) {
+            mainContent.setAlpha(0);
+            mainContent.animate().alpha(1).setDuration(ll);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public String toString() {
+        return "MainActivity[mAppBarConfiguration=" + mAppBarConfiguration + "]";
+    }
 }
--- app/src/main/java/io/hifii/drawertest/ui/home/HomeFragment.java
+++ app/src/main/java/io/hifii/drawertest/ui/home/HomeFragment.java
@@ -1,83 +1,73 @@
 package io.hifii.drawertest.ui.home;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.activity.*;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.*;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import androidx.appcompat.app.*;
-import androidx.activity.*;
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.widget.ImageView;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.google.android.material.color.DynamicColors;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.shape.MaterialShapeDrawable;
-import com.google.android.material.snackbar.Snackbar;
-
-import com.google.android.material.color.DynamicColors;
-import com.google.android.material.color.MaterialColors;
 import io.hifii.drawertest.R;
-
 import io.hifii.drawertest.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-  private FragmentHomeBinding binding;
-  ConstraintLayout cs;
+    private FragmentHomeBinding binding;
+    ConstraintLayout cs;
 
-  public View onCreateView(
-      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+    public int l = 0;
+    private static final int addL = 350;
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-    final TextView textView = binding.textHome;
-    homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-    ConstraintLayout ci = (ConstraintLayout) root.findViewById(R.id.csTest);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    return root;
-  }
+        ImageView iv = (ImageView) root.findViewById(R.id.te);
 
-  // public static int harmonizeWithPrimary(@NonNull Context context, int colorToHarmonize) {
-  //    return harmonize(
-  //        colorToHarmonize,
-  //        // getColor(context, R.attr.primaryColor, MaterialColors.class.getCanonicalName()));
-  //
-  //
-  //  public static int harmonize(int colorToHarmonize, int colorToHarmonizeWith) {
-  //    return Blend.harmonize(colorToHarmonize, colorToHarmonizeWith);
-  //  }
-  
-  
-  @Override
-  public void onDestroyView() {
-    super.onDestroyView();
-    binding = null;
-  }
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(1.1f, 1.65f);
+        valueAnimator.setDuration(5000);
+        valueAnimator.addUpdateListener(
+                new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        iv.setScaleX((Float) animation.getAnimatedValue());
+                        iv.setScaleY((Float) animation.getAnimatedValue());
+                    }
+                });
+        valueAnimator.setRepeatCount(Animation.INFINITE);
+        valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        valueAnimator.start();
+        valueAnimator.start();
+
+        RelativeLayout rl = (RelativeLayout) root.findViewById(R.id.rlOne);
+
+        l = ViewGroup.LayoutParams.MATCH_PARENT;
+
+        rl.getLayoutParams().height = l + addL;
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        ConstraintLayout ci = (ConstraintLayout) root.findViewById(R.id.csTest);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
